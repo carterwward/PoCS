525709234f167a3cc1e05978f2a196f78213b161
carterwward <Carter.Ward@uvm.edu>
2021-10-14 17:42:29 -0400

    faster

--- assignment_7/assignment_7.jl
+++ assignment_7/assignment_7.jl
@@ -1,37 +1,26 @@
+using BenchmarkTools
+using Plots
+
 get_val(r, γ) = (1- r)^(-1/(γ - 1))
 
 n = 1000
-N = 1000000
 Ns = [10, 100, 1000, 10000, 100000, 1000000]
 γ = 5/2
 
-Ni_dict = Dict(i => Array{Float64,2}(undef, n, i) for i in Ns)
-# Ni_dict = Dict()
-
-# for key in keys(Ni_dict)
-#     println(size(Ni_dict[key]))
-# end
 N_maxs = Array{Float64,2}(undef, 6, 1000)
-@benchmark for N in Ns
-    # results = [[] for _ in 1:Threads.nthreads()];
-    # Threads.@threads for a in 1:n
-    for a in 1:n
-        for b in 1:N
-            # results[Threads.threadid()] = round(get_val(np.random.uniform(), gamma))
-            Ni_dict[N][a,b] = round(get_val(rand(), γ))
-        end
+
+for (index, N) in pairs(Ns)
+    Ni_results = Array{Float64}(undef, n)
+    Threads.@threads for a in 1:n
+        Ni_results[a] = maximum(round.((1 .- rand(Float64, N)).^(-1/(γ - 1))))
     end
+    N_maxs[index, :]= Ni_results
 end
-do_plot(k) = plot(scatter(x=x_ax, y=Ni_dict[k]))
-p = [do_plot(10) do_plot(100) do_plot(1000) do_plot(100000) do_plot(1000000)]
-for (key, value) in Ni_dict
-    vals, inds = findmax(value, dims=2)
-    x_ax = [i for i in 1:1000]
-    p_i = plot(scatter(x=x_ax, y=vals))
-    # p_i
-    append!(p, p_i)
-end
-PlotlyJS.relayout!(p)
-p
+
+plot([1:1000], N_maxs', layout = (3, 2))
+
+k_max_means = mean(N_maxs, dims=2)
+
+scatter(log10.(Ns), log10.(k_max_means))
 
 
